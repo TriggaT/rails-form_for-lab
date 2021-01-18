b33a4351104e88f540c1d4cdaadaad7200392d6a
Trayshawn <traytwebb14@yahoo.com>
2021-01-17 18:22:25 -0600

    commit tree 7be977b8895137d51c84fee02852f826d0d36535

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.create(title: "Software Engineering", room_number: "101")
+SchoolClass.create(title: "Biochemistry", room_number: "404")
